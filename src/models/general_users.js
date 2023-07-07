f78e9d0767d00555ecd5245aa4b0c79b01eda2a4
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-07 14:02:10 +1000

    feat!: general users schema added

---
+++ src/models/general_users.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const GeneralUsersSchema = new Schema({
+  fullName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    minLength: 8,
+    required: true
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  },
+  jwt: {
+    jwt: String,
+    expiry: {
+        type: Date,
+        expires: Date.now() + 7
+    }
+  }
+});
+
+const GeneralUser = mongoose.model('GeneralUser', GeneralUsersSchema);
+
+module.exports = GeneralUser
