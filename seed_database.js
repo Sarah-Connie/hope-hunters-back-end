b2b2e94f82d2e6a94cbe8d1c2f9092ecba35eaf2
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-10 16:48:35 +1000

    feat: seedDatabase command extracted to seperate file now that it is known to function as expected

--- seed_database.js
+++ seed_database.js
@@ -1,9 +1,22 @@
-const { seedDatabase } = require("./database")
+const { databaseConnector, seedDatabase, databaseDisconnector } = require("./database")
 
+databaseConnector(databaseURL).then(() => {
+	console.log("Database connection established.")
+}).catch(error => {
+	console.log("Unable to establish database connection.")
+	console.log(error)
+})
 
 seedDatabase().then(() => {
     console.log('Database seeded.')
 }).catch(error => {
     console.log('Unable to seed data.')
+    console.log(error)
+})
+
+databaseDisconnector.then(() => {
+    console.log('Database connection closed.')
+}).catch(error => {
+    console.log('Unable to close database connection.')
     console.log(error)
 })
