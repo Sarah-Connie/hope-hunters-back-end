96c390302f107a8d9b459f1ca07d2df5e01b9b6a
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-12 16:44:41 +1000

    feat!: functions added to encrypt and decrypt data

---
+++ src/helper_functions/cryptography_management.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const crypto = require('crypto');
+let encAlgorithm = 'aes-256-cbc';
+let encPrivateKey = crypto.scryptSync(process.env.ENC_KEY, SECRET_SALT, 32);
+let encIV = crypto.scryptSync(process.env.ENC_IV, SECRET_SALT, 16);
+let cipher = crypto.createCipheriv(encAlgorithm, encPrivateKey, encIV);
+let decipher = crypto.createDecipheriv(encAlgorithm, encPrivateKey, encIV);
+
+// Encrypt a plaintext string
+function encryptString(data){
+    return cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
+}
+
+// Decrypt an encoded string to a plaintext string
+function decryptString(data){
+    return decipher.update(data, 'hex', 'utf8') + decipher.final('utf8');
+}
+
+module.exports = {
+    encryptString,
+    decryptString
+}
