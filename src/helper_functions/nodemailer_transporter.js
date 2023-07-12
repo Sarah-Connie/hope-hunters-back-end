8f570797cc8fddeadb7a9f053eee8fefc05b930b
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-12 15:12:12 +1000

    feat: nodemailer transporter extracted to seperate file

---
+++ src/helper_functions/nodemailer_transporter.js
@@ -0,0 +1,9 @@
+const transporter = nodemailer.createTransport(
+    sendgridTransport({
+        auth:{
+            api_key: process.env.SENDGRID_API_KEY
+        }
+    })
+);
+
+module.exports = transporter
