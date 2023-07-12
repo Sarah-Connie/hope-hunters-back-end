a1f8358059ecbc4dc37f0497abcbaba2b9221db9
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-12 17:32:34 +1000

    feat!: middleware added to validate a request based on a valid JWT being supplied in the header

---
+++ src/middlewares/validate_JWT.js
@@ -0,0 +1,25 @@
+const { decryptString } = require('../helper_functions/cryptography_management');
+const { verifyJWT } = require('../helper_functions/JWT_management');
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const validateRequest = (request, response, next) => {
+    // Get JWT from request header and decrypt it
+    let suppliedToken = decryptString(request.headers.authorization.split(" ")[1]);
+
+    try {
+        // If no JWT send response with an error message
+        if (!suppliedToken) {
+            response.status(400).json({error: 'User authorisation could not be verified.'})
+        }
+        // Verify JWT
+        verifyJWT(suppliedToken);
+        // If no errors call next()
+        return next();
+    } catch (error) {
+        next(error);
+    }
+}
