1965e054b5f71a1b3b3605f7dcc3cf4a5b271c64
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-12 17:50:18 +1000

    feat!: login route added for general and polce users. Verifies email, password and if a JWT exists on the document (if not, send verification email again)

---
+++ src/controllers/users_login.js
@@ -0,0 +1,70 @@
+const { GeneralUser } = require('../models/general_users');
+const { PoliceUser } = require('../models/police_users')
+
+const { transporter } = require('../helper_functions/nodemailer_transporter')
+
+
+const login = async (request, response, next) => {
+    if ((request.body.email).includes('@police.nsw.gov.au')) {
+        await PoliceUser.findOne({email: request.body.email}, function(error, user) {
+            if (error) {
+                response.status(500).json({message: 'Server unable to connect.', error: error.message})
+            } else if (!user) {
+                response.status(401).json({message: 'User not found.'})
+            } else if (request.body.password != user.password) {
+                response.status(401).json({message: 'Incorrect password.'})
+            } else if (!user.jwt.jwt) {
+                const emailActivate = {
+                    from: "hopehelperes@gmail.com",
+                    to: request.body.email,
+                    subject: "Verify Email Address - Hope Helpers",
+                    text: 'Welcom '+ request.body.stationNameName +',\n\n' + 
+                            'Please verify your email address by clicking the link: \nhttp:\/\/' 
+                            + request.headers.host + '\/police\/' + '\/confirmation\/' + request.body.email 
+                            + '\n\nThank You!\n' 
+                    };
+                
+                    transporter.sendMail(emailActivate, function (error) {
+                        if (error) { 
+                            response.status(500).json({error:'Something went wrong. Unable to send email.'});
+                        }
+                        response.status(200).send('A verification email has been sent to ' + request.body.email + '.');
+                    });
+            } else {
+                response.status(200).send(user.jwt.jwt);
+            }
+        })
+    } else {
+        await GeneralUser.findOne({email: request.body.email}, function(error, user) {
+            if (error) {
+                response.status(500).json({message: 'Server unable to connect.', error: error.message})
+            } else if (!user) {
+                response.status(401).json({message: 'User not found.'})
+            } else if (request.body.password != user.password) {
+                response.status(401).json({message: 'Incorrect password.'})
+            } else if (!user.jwt.jwt) {
+                const emailActivate = {
+                    from: "hopehelperes@gmail.com",
+                    to: request.body.email,
+                    subject: "Verify Email Address - Hope Helpers",
+                    text: 'Welcom '+ request.body.stationNameName +',\n\n' + 
+                            'Please verify your email address by clicking the link: \nhttp:\/\/' 
+                            + request.headers.host + '\/general\/' + '\/confirmation\/' + request.body.email 
+                            + '\n\nThank You!\n' 
+                    };
+                
+                    transporter.sendMail(emailActivate, function (error) {
+                        if (error) { 
+                            response.status(500).json({error:'Something went wrong. Unable to send email.'});
+                        }
+                        response.status(200).send('A verification email has been sent to ' + request.body.email + '.');
+                    });
+            } else {
+                response.status(200).send(user.jwt.jwt);
+            }
+        })
+    }
+};
+
+
+module.exports = { login }
