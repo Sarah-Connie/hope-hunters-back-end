b672832b523580d88fedf8cbf296d2aaa116b741
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-12 17:50:36 +1000

    feat: login route added to userRouter

--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -1,15 +1,18 @@
 const { signup } = require('../controllers/users_signup');
 const { generalUsersConfirmation } = require('../controllers/verify_general_user');
 const { policeUsersConfirmation } = require('../controllers/verify_police_user');
+const { login } = require('../controllers/users_login')
 
 const express = require('express');
+const { getLogger } = require('nodemailer/lib/shared');
 const usersRouter = express.Router();
 
 
 usersRouter.post('/signup', signup);
 
 usersRouter.put('/signup/general/confirmation', generalUsersConfirmation);
-usersRouter.pup('/signup/police/confirmation', policeUsersConfirmation);
+usersRouter.put('/signup/police/confirmation', policeUsersConfirmation);
 
+usersRouter.post('./login', login)
 
 module.exports = usersRouter;
