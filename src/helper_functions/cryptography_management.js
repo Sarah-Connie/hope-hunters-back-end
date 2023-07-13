323155bcfa4b72d19a8d6a65591fe45ee51ec156
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-13 15:23:34 +1000

    fix: correct access to environmental variable added for SECRET_SALT

--- src/helper_functions/cryptography_management.js
+++ src/helper_functions/cryptography_management.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 const crypto = require('crypto');
 let encAlgorithm = 'aes-256-cbc';
-let encPrivateKey = crypto.scryptSync(process.env.ENC_KEY, SECRET_SALT, 32);
-let encIV = crypto.scryptSync(process.env.ENC_IV, SECRET_SALT, 16);
+let encPrivateKey = crypto.scryptSync(process.env.ENC_KEY, process.env.SECRET_SALT, 32);
+let encIV = crypto.scryptSync(process.env.ENC_IV, process.env.SECRET_SALT, 16);
 let cipher = crypto.createCipheriv(encAlgorithm, encPrivateKey, encIV);
 let decipher = crypto.createDecipheriv(encAlgorithm, encPrivateKey, encIV);
 
