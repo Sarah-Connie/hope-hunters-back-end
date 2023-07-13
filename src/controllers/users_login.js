6160afa7a44827b2ca224a9574a44d9ebd210a29
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-13 13:13:37 +1000

    feat: login route altered to make fetch() request to update user's jwt when login is successful

--- src/controllers/users_login.js
+++ src/controllers/users_login.js
@@ -4,7 +4,7 @@ const { PoliceUser } = require('../models/police_users')
 const { transporter } = require('../helper_functions/nodemailer_transporter')
 
 
-const login = async (request, response, next) => {
+const login = async (request, response) => {
     if ((request.body.email).includes('@police.nsw.gov.au')) {
         await PoliceUser.findOne({email: request.body.email}, function(error, user) {
             if (error) {
@@ -13,14 +13,14 @@ const login = async (request, response, next) => {
                 response.status(401).json({message: 'User not found.'})
             } else if (request.body.password != user.password) {
                 response.status(401).json({message: 'Incorrect password.'})
-            } else if (!user.jwt.jwt) {
+            } else if (!user.jwt) {
                 const emailActivate = {
                     from: "hopehelperes@gmail.com",
                     to: request.body.email,
                     subject: "Verify Email Address - Hope Helpers",
-                    text: 'Welcom '+ request.body.stationNameName +',\n\n' + 
+                    text: 'Welcome '+ request.body.stationNameName +',\n\n' + 
                             'Please verify your email address by clicking the link: \nhttp:\/\/' 
-                            + request.headers.host + '\/police\/' + '\/confirmation\/' + request.body.email 
+                            + request.headers.host + '\/police' + '\/confirmation\/' + request.body.email 
                             + '\n\nThank You!\n' 
                     };
                 
@@ -31,7 +31,13 @@ const login = async (request, response, next) => {
                         response.status(200).send('A verification email has been sent to ' + request.body.email + '.');
                     });
             } else {
-                response.status(200).send(user.jwt.jwt);
+                try {
+                    let url = 'http://' + request.headers.host + '/refresh-token/' + user.jwt
+                    const updateToken = fetch(url)
+                    response.status(200).json({message: 'Login successful.'});
+                } catch (error) {
+                    throw new Error(error)
+                } 
             }
         })
     } else {
@@ -42,14 +48,14 @@ const login = async (request, response, next) => {
                 response.status(401).json({message: 'User not found.'})
             } else if (request.body.password != user.password) {
                 response.status(401).json({message: 'Incorrect password.'})
-            } else if (!user.jwt.jwt) {
+            } else if (!user.jwt) {
                 const emailActivate = {
                     from: "hopehelperes@gmail.com",
                     to: request.body.email,
                     subject: "Verify Email Address - Hope Helpers",
                     text: 'Welcom '+ request.body.stationNameName +',\n\n' + 
                             'Please verify your email address by clicking the link: \nhttp:\/\/' 
-                            + request.headers.host + '\/general\/' + '\/confirmation\/' + request.body.email 
+                            + request.headers.host + '\/general' + '\/confirmation\/' + request.body.email 
                             + '\n\nThank You!\n' 
                     };
                 
@@ -60,7 +66,13 @@ const login = async (request, response, next) => {
                         response.status(200).send('A verification email has been sent to ' + request.body.email + '.');
                     });
             } else {
-                response.status(200).send(user.jwt.jwt);
+                try {
+                    let url = 'http://' + request.headers.host + '/refresh-token/' + user.jwt
+                    const updateToken = fetch(url)
+                    response.status(200).json({message: 'Login successful.'});
+                } catch (error) {
+                    throw new Error(error)
+                } 
             }
         })
     }
