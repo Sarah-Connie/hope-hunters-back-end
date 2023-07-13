8aa3a996b216b826abc82692e55807266dcaa99f
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-13 13:17:37 +1000

    fix: user email verfication new parameter to insert when updating document changed to insert jwt only (no expiry)

--- src/controllers/verify_general_user.js
+++ src/controllers/verify_general_user.js
@@ -5,18 +5,16 @@ const { generateJWT } = require('../helper_functions/JWT_management');
 const { encryptString } = require('../helper_functions/cryptography_management');
 
 
-const generalUsersConfirmation = async (request, response, next) => {
+const generalUsersConfirmation = async (request, response) => {
     // Generate JWT and encrypt it
     const jwtoken = encryptString(generateJWT(request.param.email)) 
-    // Generate date for expiry separate to JWT
-    const expiry = Date.now() + 7
 
     // Find the correct GeneralUser document using the supplied email address from sign up and add the new JWT and expiry
-    let addJWT = await GeneralUser.findOneAndUpdate(request.param.email, {jwt: {jwt: jwtoken, expiry: expiry}}, {new: true})
+    let addJWT = await GeneralUser.findOneAndUpdate(request.param.email, {jwt: jwtoken}, {new: true})
                     .catch(error => { 
                         response.status(401).json({error:'Unable to verify user.'});
                     }) 
-    
+                    
     if (addJWT) {
         // If JWT was successfully added to a user document return the JWT
         response.status(200).send(jwtoken)
--- src/controllers/verify_police_user.js
+++ src/controllers/verify_police_user.js
@@ -5,14 +5,12 @@ const { generateJWT } = require('../helper_functions/JWT_management');
 const { encryptString } = require('../helper_functions/cryptography_management');
 
 
-const policeUsersConfirmation = async (request, response, next) => {
+const policeUsersConfirmation = async (request, response) => {
     // Generate JWT and encrypt it
     const jwtoken = encryptString(generateJWT(request.param.email)) 
-    // Generate date for expiry separate to JWT 
-    const expiry = Date.now() + 7
 
     // Find the correct PoliceUser document using the supplied email address from sign up and add the new JWT and expiry
-    let addJWT = await PoliceUser.findOneAndUpdate(request.param.email, {jwt: {jwt: jwtoken, expiry: expiry}}, {new: true})
+    let addJWT = await PoliceUser.findOneAndUpdate(request.param.email, {jwt: jwtoken}, {new: true})
                     .catch(error => { 
                         response.status(401).json({error:'Unable to verify user.'});
                     }) 
