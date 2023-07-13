3f5025665cc799901d14e8e8ff9a7d4e04dc9c5d
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-13 14:42:54 +1000

    fix: response message for error in updating JWT in use document altered to be more descriptive

--- src/controllers/refresh_JWT.js
+++ src/controllers/refresh_JWT.js
@@ -14,9 +14,9 @@ const refreshJWT = async (request, response) => {
 
     // Try to find the user document by querying the general users collection with the old token
     let updateGeneralToken = await GeneralUser.findOneAndUpdate({jwt: request.params.token}, {jwt: newToken}, {new: true}) 
-    .catch(error => { 
-        response.status(401).json({error:'Unable to verify user.'});
-    })
+                                .catch(error => { 
+                                    response.status(401).json({error:'Unable to update JWT in user document.'});
+                                })
     if (updateGeneralToken) {
         // If JWT was successfully updated on a document in the general users collection return the new JWT
         response.status(200).send(newToken)
@@ -24,9 +24,9 @@ const refreshJWT = async (request, response) => {
     
     // // Otherwise, try to find the user document by querying the police users collection with the old token
     let updatePoliceToken = await PoliceUser.findOneAndUpdate({jwt: request.params.token}, {jwt: newToken}, {new: true}) 
-    .catch(error => { 
-        response.status(401).json({error:'Unable to verify user.'});
-    })
+                                .catch(error => { 
+                                    response.status(401).json({error:'Unable to update JWT in user document.'});
+                                })
     if (updatePoliceToken) {
         // If JWT was successfully updated on a document in the police users collection return the new JWT
         response.status(200).send(newToken)
