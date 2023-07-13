15d4e82444b80c2f56bdff8a43d444cc7c8ec114
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-13 16:24:06 +1000

    feat: comments added

--- src/helper_functions/JWT_management.js
+++ src/helper_functions/JWT_management.js
@@ -4,10 +4,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
+// Generate a JWT with the an email address. Access secret in an environmental variable. Set expiry for 7 days.
 function generateJWT(userDetails) {
     return jwt.sign({email: userDetails}, process.env.JWT_SECRET, {expiresIn: '7d'});
 }
 
+// Verify a provided JWT is valid with the secret stored in an environmental variable.
 function verifyJWT(JWT) {
     try{
         return jwt.verify(JWT, process.env.JWT_SECRET_KEY);
