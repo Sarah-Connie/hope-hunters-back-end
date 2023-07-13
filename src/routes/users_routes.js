e97c9c3472f52d105535eca4a61e87fb20f9697c
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-13 13:10:24 +1000

    feat: refreshjwt route added to userRouter

--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -1,18 +1,20 @@
 const { signup } = require('../controllers/users_signup');
 const { generalUsersConfirmation } = require('../controllers/verify_general_user');
 const { policeUsersConfirmation } = require('../controllers/verify_police_user');
-const { login } = require('../controllers/users_login')
+const { login } = require('../controllers/users_login');
+const { refreshJWT } = require('../controllers/refresh_JWT');
 
 const express = require('express');
-const { getLogger } = require('nodemailer/lib/shared');
 const usersRouter = express.Router();
 
 
 usersRouter.post('/signup', signup);
 
-usersRouter.put('/signup/general/confirmation', generalUsersConfirmation);
-usersRouter.put('/signup/police/confirmation', policeUsersConfirmation);
+usersRouter.put('/signup/general/confirmation/:email', generalUsersConfirmation);
+usersRouter.put('/signup/police/confirmation/:email', policeUsersConfirmation);
 
 usersRouter.post('./login', login)
+
+usersRouter.put('./login/refresh-token/:jwt', refreshJWT)
 
 module.exports = usersRouter;
