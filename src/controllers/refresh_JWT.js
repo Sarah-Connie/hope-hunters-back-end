9d2fe9c3669fbe37df1eca17d0035adc73096123
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-16 17:01:29 +1000

    fix: return statements added

--- src/controllers/refresh_JWT.js
+++ src/controllers/refresh_JWT.js
@@ -4,7 +4,7 @@ const refreshJWT = async (request, response) => {
     let suppliedToken = decryptString(request.params.token);
     // If no JWT send response with an error message
     if (!suppliedToken) {
-        response.status(400).json({error: 'User authorisation could not be verified.'})
+        return response.status(400).json({error: 'User authorisation could not be verified.'})
     }
     // Verify JWT
     verifyJWT(suppliedToken)
@@ -15,25 +15,25 @@ const refreshJWT = async (request, response) => {
     // Try to find the user document by querying the general users collection with the old token
     let updateGeneralToken = await GeneralUser.findOneAndUpdate({jwt: request.params.token}, {jwt: newToken}, {new: true}) 
                                 .catch(error => { 
-                                    response.status(401).json({error:'Unable to update JWT in user document.'});
+                                    return response.status(401).json({error:'Unable to update JWT in user document.'});
                                 })
     if (updateGeneralToken) {
         // If JWT was successfully updated on a document in the general users collection return the new JWT
-        response.status(200).send(newToken)
+        return response.status(200).send(newToken)
     }
     
     // // Otherwise, try to find the user document by querying the police users collection with the old token
     let updatePoliceToken = await PoliceUser.findOneAndUpdate({jwt: request.params.token}, {jwt: newToken}, {new: true}) 
                                 .catch(error => { 
-                                    response.status(401).json({error:'Unable to update JWT in user document.'});
+                                    return response.status(401).json({error:'Unable to update JWT in user document.'});
                                 })
     if (updatePoliceToken) {
         // If JWT was successfully updated on a document in the police users collection return the new JWT
-        response.status(200).send(newToken)
+        return response.status(200).send(newToken)
     } else {
-    // Otherwise return an error
-    response.status(400).json({error: 'Unable to update JWT'})
-    }
-
-    
+        // Otherwise return an error
+        return response.status(400).json({error: 'Unable to update JWT'})
+    } 
 }
+
+module.exports = { refreshJWT }
