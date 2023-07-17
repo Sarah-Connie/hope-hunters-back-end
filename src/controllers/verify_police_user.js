3e532ccd3236ebdecec772f45fba6c19e6251c11
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-17 17:15:06 +1000

    fix: database query corrected to search for user by their email address

--- src/controllers/verify_police_user.js
+++ src/controllers/verify_police_user.js
@@ -6,17 +6,17 @@ const { encryptString } = require('../helper_functions/cryptography_management')
 
 const policeUsersConfirmation = async (request, response) => {
     // Generate JWT and encrypt it
-    const jwtoken = encryptString(generateJWT(request.param.email)) 
+    const jwtoken = encryptString(generateJWT(request.params.email)) 
 
     // Find the correct PoliceUser document using the supplied email address from sign up and add the new JWT and expiry
-    let addJWT = await PoliceUser.findOneAndUpdate(request.param.email, {jwt: jwtoken}, {new: true})
+    let addJWT = await PoliceUser.findOneAndUpdate({email: request.params.email}, {jwt: jwtoken}, {new: true})
                     .catch(error => { 
-                        return response.status(401).json({error:'Unable to verify user.'});
+                        return response.status(404).json({error:'Unable to find user document.'});
                     }) 
     
     if (addJWT) {
         // If JWT was successfully added to a user document return the JWT
-        return response.status(200).send(addJWT)
+        return response.status(200).json({token: jwtoken})
     } else {
         // Otherwise return an error
         return response.status(400).json({error: 'User account could not be verified'})
