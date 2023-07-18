5f655fc11feadfa180b387cf0310c032e795ed1a
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-18 13:49:10 +1000

    feat: comments added to login function. Function altered to correctly handle errors when failing to locate user, incorrect password, incorrect email

--- src/controllers/users_login.js
+++ src/controllers/users_login.js
@@ -1,80 +1,90 @@
 const { GeneralUser } = require('../models/general_users');
-const { PoliceUser } = require('../models/police_users')
+const { PoliceUser } = require('../models/police_users');
 
-const { transporter } = require('../helper_functions/nodemailer_transporter')
+const { transporter } = require('../helper_functions/nodemailer_transporter');
+
+const { decryptString } = require('../helper_functions/cryptography_management');
 
 
 const login = async (request, response) => {
+    // Identify if user is trying to login in as a police or general user by checking the supplied email address
+    // If true, user is a police user
     if ((request.body.email).includes('@police.nsw.gov.au')) {
-        await PoliceUser.findOne({email: request.body.email}, function(error, user) {
-            if (error) {
-                response.status(500).json({message: 'Server unable to connect.', error: error.message})
-            } else if (!user) {
-                response.status(401).json({message: 'User not found.'})
-            } else if (request.body.password != user.password) {
-                response.status(401).json({message: 'Incorrect password.'})
-            } else if (!user.jwt) {
-                const emailActivate = {
-                    from: "hopehelperes@gmail.com",
-                    to: request.body.email,
-                    subject: "Verify Email Address - Hope Helpers",
-                    text: 'Welcome '+ request.body.stationNameName +',\n\n' + 
-                            'Please verify your email address by clicking the link: \nhttp:\/\/' 
-                            + request.headers.host + '\/police' + '\/confirmation\/' + request.body.email 
-                            + '\n\nThank You!\n' 
-                    };
-                
-                    transporter.sendMail(emailActivate, function (error) {
-                        if (error) { 
-                            response.status(500).json({error:'Something went wrong. Unable to send email.'});
-                        }
-                        response.status(200).send('A verification email has been sent to ' + request.body.email + '.');
-                    });
-            } else {
-                try {
-                    let url = 'http://' + request.headers.host + '/refresh-token/' + user.jwt
-                    const updateToken = fetch(url)
-                    response.status(200).json({message: 'Login successful.'});
-                } catch (error) {
-                    throw new Error(error)
-                } 
-            }
-        })
+        // Search policeusers collection with the provided email address
+        let user = await PoliceUser.findOne({email: request.body.email})
+                            .catch(error => {response.status(404).json({error: 'Unable access collection.'})});
+
+        // If the user is not found, email address must be incorrect or invalid
+        if (!user) {
+            return response.status(400).json({error: 'Incorrect email address.'})
+        // Check that supplied password matches the password stored in the database
+        } else if (encryptString(request.body.password) != user.password) {
+            return response.status(400).json({error: 'Incorrect password.'})
+        // Check user document contains a JWT. If not, user has not verified their email address. Send new verification link to the supplied email address.
+        } else if (!user.jwt) {
+            // Set up verification email details for nodemailer/sendgrid
+            const emailActivate = {
+                from: "hopehelpersaus@gmail.com",
+                to: request.body.email,
+                subject: "Verify Email Address - Hope Helpers",
+                text: `Hello `+ request.body.fullName + `,\n\n` + 
+                        `Please verify your email address by clicking the link: \nhttp:\/\/` 
+                        + request.headers.host + `\/police` + `\/confirmation\/` + request.body.email 
+                        + `\n\nThank You!\n` 
+                };
+                 
+            // Call nodemailer to send verification email to provided address
+            transporter.sendMail(emailActivate, function (error, result) {
+                if (error) { 
+                    console.log(error)
+                    return response.status(400).json({error:'Something went wrong. Unable to send email.'});
+                } else {
+                    console.log(result)
+                    return response.status(200).json({message: 'A verification email has been sent to ' + request.body.email + '.'});
+                }
+            });
+        // If validations above pass, login the user and add the jwt to the response.body
+        } else {
+            return response.status(200).json({message: 'Login successful.', token: user.jwt});
+        }
     } else {
-        await GeneralUser.findOne({email: request.body.email}, function(error, user) {
-            if (error) {
-                response.status(500).json({message: 'Server unable to connect.', error: error.message})
-            } else if (!user) {
-                response.status(401).json({message: 'User not found.'})
-            } else if (request.body.password != user.password) {
-                response.status(401).json({message: 'Incorrect password.'})
-            } else if (!user.jwt) {
-                const emailActivate = {
-                    from: "hopehelperes@gmail.com",
-                    to: request.body.email,
-                    subject: "Verify Email Address - Hope Helpers",
-                    text: 'Welcom '+ request.body.stationNameName +',\n\n' + 
-                            'Please verify your email address by clicking the link: \nhttp:\/\/' 
-                            + request.headers.host + '\/general' + '\/confirmation\/' + request.body.email 
-                            + '\n\nThank You!\n' 
-                    };
-                
-                    transporter.sendMail(emailActivate, function (error) {
-                        if (error) { 
-                            response.status(500).json({error:'Something went wrong. Unable to send email.'});
-                        }
-                        response.status(200).send('A verification email has been sent to ' + request.body.email + '.');
-                    });
-            } else {
-                try {
-                    let url = 'http://' + request.headers.host + '/refresh-token/' + user.jwt
-                    const updateToken = fetch(url)
-                    response.status(200).json({message: 'Login successful.'});
-                } catch (error) {
-                    throw new Error(error)
-                } 
-            }
-        })
+        // Search generalusers collection with the provided email address
+        let user = await GeneralUser.findOne({email: request.body.email})
+                            .catch(error => {response.status(404).json({error: 'Unable access collection.'})});
+
+        // If the user is not found, email address must be incorrect or invalid
+        if (!user) {
+            return response.status(400).json({error: 'Incorrect email address.'})
+        // Check that supplied password matches the password stored in the database
+        } else if (request.body.password != decryptString(user.password)) {
+            return response.status(400).json({error: 'Incorrect password.'})
+        // Check user document contains a JWT. If not, user has not verified their email address. Send new verification link to the supplied email address.
+        } else if (!user.jwt) {
+            // Set up verification email details for nodemailer/sendgrid
+            const emailActivate = {
+                from: "hopehelpersaus@gmail.com",
+                to: request.body.email,
+                subject: "Verify Email Address - Hope Helpers",
+                text: `Hello `+ request.body.fullName + `,\n\n` + 
+                        `Please verify your email address by clicking the link: \nhttp:\/\/` 
+                        + request.headers.host + `\/general` + `\/confirmation\/` + request.body.email 
+                        + `\n\nThank You!\n` 
+            };
+     
+            // Call nodemailer to send verification email to provided address
+            transporter.sendMail(emailActivate, function (error, result) {
+                if (error) { 
+                    console.log(error)
+                    return response.status(400).json({error:'Something went wrong. Unable to send email.'});
+                } else {
+                    console.log(result)
+                    return response.status(200).json({message: 'A verification email has been sent to ' + request.body.email + '.'});
+                }
+            });
+        // If validations above pass, login the user and add the jwt to the response.body
+        } else {
+            return response.status(200).json({message: 'Login successful.', token: user.jwt});
+        }
     }
 };
 
