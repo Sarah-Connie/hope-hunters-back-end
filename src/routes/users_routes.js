127dbd2f40c2ff86addb8a0e8cff6698abd740dd
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-19 17:04:34 +1000

    feat: users/update and users/delete routes added to useresRouter

--- src/routes/users_routes.js
+++ src/routes/users_routes.js
@@ -4,20 +4,28 @@ const { policeUsersConfirmation } = require('../controllers/verify_police_user')
 const { login } = require('../controllers/users_login');
 const { refreshJWT } = require('../controllers/refresh_JWT');
 const { getUsers } = require('../controllers/users_all');
+const { updateUsers } = require('../controllers/users_update');
+const { deleteUsers } = require('../controllers/users_delete');
+
+const { validateRequest } = require('../middlewares/validate_JWT');
 
 const express = require('express');
 const usersRouter = express.Router();
 
 
-usersRouter.get('/', getUsers)
+usersRouter.get('/', getUsers);
 
 usersRouter.post('/signup', signup);
 
 usersRouter.put('/signup/general/confirmation/:email', generalUsersConfirmation);
 usersRouter.put('/signup/police/confirmation/:email', policeUsersConfirmation);
 
-usersRouter.post('/login', login)
+usersRouter.post('/login', login);
 
-usersRouter.put('/login/refresh-token', refreshJWT)
+usersRouter.put('/login/refresh-token', refreshJWT);
+
+usersRouter.put('/update', validateRequest, updateUsers);
+
+usersRouter.delete('/delete', validateRequest, deleteUsers);
 
 module.exports = usersRouter;
