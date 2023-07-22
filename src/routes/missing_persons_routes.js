4e722118f763ec61dc3fb16862fb24a1f424f012
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-22 14:04:06 +1000

    feat: missing/update/:id route added to missingRouter

--- src/routes/missing_persons_routes.js
+++ src/routes/missing_persons_routes.js
@@ -1,5 +1,6 @@
 const { getMissing } = require('../controllers/missing_all');
 const { addMissingPerson } = require('../controllers/new_missing_person');
+const { updateMissing } = require('../controllers/missing_update');
 
 const { validateRequest } = require('../middlewares/validate_JWT');
 const { getUserStatus } = require('../middlewares/userStatus');
@@ -11,5 +12,7 @@ const missingRouter = express.Router();
 missingRouter.get('/', getMissing);
 
 missingRouter.post('/new', validateRequest, getUserStatus, addMissingPerson);
+
+missingRouter.put('/update/:id', validateRequest, getUserStatus, updateMissing);
 
 module.exports = missingRouter;
