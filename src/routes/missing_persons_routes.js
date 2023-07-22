6731f9daa997c85784f726cf1fd0ffecd16235b4
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-22 12:57:35 +1000

    feat: getUserStatus middleware added to /missing/new route

--- src/routes/missing_persons_routes.js
+++ src/routes/missing_persons_routes.js
@@ -1,11 +1,15 @@
 const { getMissing } = require('../controllers/missing_all');
+const { addMissingPerson } = require('../controllers/new_missing_person');
 
 const { validateRequest } = require('../middlewares/validate_JWT');
+const { getUserStatus } = require('../middlewares/userStatus');
 
 const express = require('express');
 const missingRouter = express.Router();
 
 
 missingRouter.get('/', getMissing);
+
+missingRouter.post('/new', validateRequest, getUserStatus, addMissingPerson);
 
 module.exports = missingRouter;
