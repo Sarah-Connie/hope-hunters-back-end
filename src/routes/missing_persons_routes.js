c402186139e89b8385c9df2fe5292819a4f69bdb
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-23 12:46:51 +1000

    feat: /delete/:id route added to missingRouter

--- src/routes/missing_persons_routes.js
+++ src/routes/missing_persons_routes.js
@@ -1,6 +1,7 @@
 const { getMissing } = require('../controllers/missing_all');
 const { addMissingPerson } = require('../controllers/new_missing_person');
 const { updateMissing } = require('../controllers/missing_update');
+const { deleteMissing } = require('../controllers/missing_delete');
 
 const { validateRequest } = require('../middlewares/validate_JWT');
 const { getUserStatus } = require('../middlewares/userStatus');
@@ -14,5 +15,7 @@ missingRouter.get('/', getMissing);
 missingRouter.post('/new', validateRequest, getUserStatus, addMissingPerson);
 
 missingRouter.put('/update/:id', validateRequest, getUserStatus, updateMissing);
+
+missingRouter.delete('/delete/:id', validateRequest, getUserStatus, deleteMissing);
 
 module.exports = missingRouter;
