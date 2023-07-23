298cfbf0095d4e13397490d9a1d1b3278708da9e
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-23 12:46:28 +1000

    feat: comments added

--- src/controllers/missing_update.js
+++ src/controllers/missing_update.js
@@ -4,19 +4,23 @@ const { MissingPerson } = require('../models/missing_persons');
 const updateMissing = async (request, response) => {
     // Find the correct missingpersons document using the _id on the url
 
+    // Search for the correct document with the id provided in the URL parameter
     let missing = await MissingPerson.findOne({_id: request.params.id})
                         .catch(error => { 
+                            // If no document found, return an error message
                             return response.status(400).json({error:'Unable to find missing document.'});
                         })
 
+    // Check that the user is a police or admin user, or is the owner of the document
     if (request.police === true || request.admin === true || request._id === missing._id) {
+        // If they are, update the document with the corresponding fields from the request.body
         let updateMissingPerson = await MissingPerson.findOneAndUpdate({_id: request.params.id}, request.body, {new: true})
                             .catch(error => { 
                                 return response.status(400).json({error:'Unable to find missing person document.'});
                             });
 
         if (updateMissingPerson) {
-            // return response.status(200).json({message: 'User details successfully updated.'})
+            // If the document was updated, return the updated document
             return response.status(200).json(updateMissingPerson)
         } else {
             // Otherwise return an error
@@ -24,6 +28,7 @@ const updateMissing = async (request, response) => {
         }
 
     } else {
+        // If the user is not the owner or an admin or police user, return an error
         return response.status(401).json({error: 'User unauthorised to alter this document.'})
     }    
 }
