5fcbdc45a0cf171465418d0b8540eaa02672a477
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-25 17:13:35 +1000

    fix: typo in comment fixed

--- src/controllers/missing_all.js
+++ src/controllers/missing_all.js
@@ -3,7 +3,7 @@ const { MissingPerson } = require('../models/missing_persons');
 
 // Get all persons documents in the missingpersons collection
 const getMissing = async (request, response) => {
-    // Get all documents in the missingpersons collection and sort them in descending order for date added
+    // Get all documents in the missingpersons collection and sort them in descending order of date added
     let missing = await MissingPerson.find({}).sort('-dateAdded')
                                 .catch(error => {
                                     return response.status(404).json({error: 'Unable to access general users documents.'})});
