5c21b66a96393ee790503a0119d4b52108a1a98a
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-07-31 16:17:49 +1000

    fix: corrected error message in unable to retrieve documents from general user to missing persons

--- src/controllers/missing_all.js
+++ src/controllers/missing_all.js
@@ -6,7 +6,7 @@ const getMissing = async (request, response) => {
     // Get all documents in the missingpersons collection and sort them in descending order of date added
     let missing = await MissingPerson.find({}).sort('-dateAdded')
                                 .catch(error => {
-                                    return response.status(404).json({error: 'Unable to access general users documents.'})});
+                                    return response.status(404).json({error: 'Unable to access missing persons documents.'})});
     // Respond with the documents
     return response.status(200).send(missing) 
 }
