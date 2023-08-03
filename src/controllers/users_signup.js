f154ffc451705966a2f9dd344ddda2d811f1a477
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-08-03 13:12:31 +1000

    fix: referer changed to origin in request.headers location on signup and login controllers

--- src/controllers/users_signup.js
+++ src/controllers/users_signup.js
@@ -43,7 +43,7 @@ const signup = async (request, response) => {
                 subject: "Verify Email Address - Hope Helpers",
                 text: `Hello `+ request.body.fullName + `,\n\n` + 
                         `Please verify your email address by clicking the link: ` 
-                        + request.headers.referer + `verified\/` + request.body.email 
+                        + request.headers.origin + `verified\/` + request.body.email 
                         + `\n\nThank You!\n` 
                 };
 
@@ -94,7 +94,7 @@ const signup = async (request, response) => {
                 subject: "Verify Email Address - Hope Helpers",
                 text: `Hello `+ request.body.fullName + `,\n\n` + 
                         `Please verify your email address by clicking the link: ` 
-                        + request.headers.referer + `verified\/` + request.body.email 
+                        + request.headers.origin + `verified\/` + request.body.email 
                         + `\n\nThank You!\n` 
                 };
                  
